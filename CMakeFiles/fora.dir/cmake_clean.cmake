6312da1d49f07e7fe4a97b5d567f2bca5743e381
ksyow <99003672+ksyow@users.noreply.github.com>
2022-10-19 16:38:11 +0800

    Add files via upload

---
+++ CMakeFiles/fora.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fora.dir/config.cpp.o"
+  "CMakeFiles/fora.dir/config.cpp.o.d"
+  "CMakeFiles/fora.dir/fora.cpp.o"
+  "CMakeFiles/fora.dir/fora.cpp.o.d"
+  "CMakeFiles/fora.dir/mylib.cpp.o"
+  "CMakeFiles/fora.dir/mylib.cpp.o.d"
+  "fora"
+  "fora.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/fora.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fora.
+# This may be replaced when dependencies are built.
